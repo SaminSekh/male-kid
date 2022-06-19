053ef24cd3f638a3736ed9b0e031964a6cfa3480
Samin sekh <103379453+SaminSekh@users.noreply.github.com>
2022-06-19 21:08:49 +0530

    Add files via upload

---
+++ javaScript/index.js
@@ -0,0 +1,163 @@
+   // functions for buy now pop up
+        const a1 = document.getElementById("bn");
+        const a2 = document.getElementById("ordr");
+        function ordrnw() {
+          if (a2.style.display === "none") {
+            a2.style.display = "flex";
+          } else{
+            a2.style.display = "none";
+          }
+        }
+        function closefun() {
+          if (a2.style.display === "flex") {
+            a2.style.display = "none";
+          } else{
+            a2.style.display = "flex";
+          }
+        }
+
+        const a = document.getElementById("id1")
+        const b = document.getElementById("id2")
+        const c = document.getElementById("id3")
+        const d = document.getElementById("id4")
+        const e = document.getElementById("id5")
+        const f = document.getElementById("id6")
+        const g = document.getElementById("id7")
+        const h = document.getElementById("id8")
+        const i = document.getElementById("id9")
+
+        function Fun1() {
+          if (a.style.display === "none") {
+            a.style.display = "flex";
+            b.style.display = "none";
+            c.style.display = "none";
+            d.style.display = "none";
+            e.style.display = "none";
+            f.style.display = "none";
+            g.style.display = "none";
+            h.style.display = "none";
+            i.style.display = "none";
+          } else {
+            a.style.display = "none";
+          }
+        }
+        function Fun2() {
+          if (b.style.display === "none") {
+            b.style.display = "flex";
+            a.style.display = "none";
+            c.style.display = "none";
+            d.style.display = "none";
+            e.style.display = "none";
+            f.style.display = "none";
+            g.style.display = "none";
+            h.style.display = "none";
+            i.style.display = "none";
+          } else {
+            b.style.display = "none";
+          }
+        }
+        function Fun3() {
+          if (c.style.display === "none") {
+            c.style.display = "flex";
+            a.style.display = "none";
+            b.style.display = "none";
+            d.style.display = "none";
+            e.style.display = "none";
+            f.style.display = "none";
+            g.style.display = "none";
+            h.style.display = "none";
+            i.style.display = "none";
+          } else {
+            c.style.display = "none";
+          }
+        }
+        function Fun4() {
+          if (d.style.display === "none") {
+            d.style.display = "flex";
+            a.style.display = "none";
+            b.style.display = "none";
+            c.style.display = "none";
+            e.style.display = "none";
+            f.style.display = "none";
+            g.style.display = "none";
+            h.style.display = "none";
+            i.style.display = "none";
+          } else {
+            d.style.display = "none";
+          }
+        }
+        function Fun5() {
+          if (e.style.display === "none") {
+            e.style.display = "flex";
+            a.style.display = "none";
+            b.style.display = "none";
+            c.style.display = "none";
+            d.style.display = "none";
+            f.style.display = "none";
+            g.style.display = "none";
+            h.style.display = "none";
+            i.style.display = "none";
+          } else {
+            e.style.display = "none";
+          }
+        }
+        function Fun6() {
+          if (f.style.display === "none") {
+            f.style.display = "flex";
+            a.style.display = "none";
+            b.style.display = "none";
+            c.style.display = "none";
+            d.style.display = "none";
+            e.style.display = "none";
+            g.style.display = "none";
+            h.style.display = "none";
+            i.style.display = "none";
+          } else {
+            f.style.display = "none";
+          }
+        }
+        function Fun7() {
+          if (g.style.display === "none") {
+            g.style.display = "flex";
+            a.style.display = "none";
+            b.style.display = "none";
+            c.style.display = "none";
+            d.style.display = "none";
+            e.style.display = "none";
+            f.style.display = "none";
+            h.style.display = "none";
+            i.style.display = "none";
+          } else {
+            g.style.display = "none";
+          }
+        }
+        function Fun8() {
+          if (h.style.display === "none") {
+            h.style.display = "flex";
+            a.style.display = "none";
+            c.style.display = "none";
+            b.style.display = "none";
+            d.style.display = "none";
+            e.style.display = "none";
+            f.style.display = "none";
+            g.style.display = "none";
+            i.style.display = "none";
+          } else {
+            h.style.display = "none";
+          }
+        }
+        function Fun9() {
+          if (i.style.display === "none") {
+            i.style.display = "flex";
+            a.style.display = "none";
+            b.style.display = "none";
+            c.style.display = "none";
+            d.style.display = "none";
+            e.style.display = "none";
+            f.style.display = "none";
+            g.style.display = "none";
+            h.style.display = "none";
+          } else {
+            i.style.display = "none";
+          }
+        }
